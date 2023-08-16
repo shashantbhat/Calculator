03f09dc7033474990d30f636ea748fbf2c1be81a
Shashant Bhat <120403073+shashantbhat@users.noreply.github.com>
2023-08-17 00:36:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var string = "";
+var buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('.box').value = string;
+    }
+    else if(e.target.innerHTML == 'C'){
+      string = "";
+      document.querySelector('.box').value = string;
+    }
+    else{
+    console.log(e.target);
+    string = string + e.target.innerHTML;
+    document.querySelector('.box').value = string;
+  }
+  })
+})
